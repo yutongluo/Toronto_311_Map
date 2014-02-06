6a2913b4a20a5facb28e6a8facf572d7ddfb9cb5
Yutong Luo <yluo1993@gmail.com>
2014-02-06 00:05:21 -0500

    Data extraction and insert into database finished. Next step: make new collection and get date inside before deleting old one. Delete old and rename new. This way ~40s downtime is avoided

--- data.js
+++ data.js
@@ -1,11 +1,28 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var url = "https://secure.toronto.ca/webwizard/ws/requests.json?start_date=2013-12-01T00:00:00Z&end_date=2013-12-31T00:00:00Z&jurisdiction_id=toronto.ca&status=open";
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url, true);
-xhr.onload = function(e) {
-	var buffer = xhr.responseText;
-	var json = JSON.parse(buffer);
-	console.log(json);
+
+/* getData: url, cb
+
+  Given the URL and call back, calls cb function with the parsed JSON 
+
+  TODO: Implement getData for different data_types and cb access.
+*/
+function getData(url, cb){
+	console.log("Extracting data from: " + url);
+	var xhr = new XMLHttpRequest();
+	var startTime = Date.now();
+	xhr.open("GET", url, true);
+	xhr.onload = function(err) {
+		var extractTime = (Date.now() - startTime) / 1000;
+		console.log("Data extracted in " + extractTime + "secs.");
+		var buffer = xhr.responseText;
+		var json = JSON.parse(buffer);
+		cb(json.service_requests);
+	}
+	xhr.onerror = function(err) {
+		console.log("ERROR: FAILED TO EXTRACT DATA FROM " + url);
+		return err;
+	}
+	xhr.send();
 }
 
-xhr.send();
+module.exports = getData;
--- mongo.js
+++ mongo.js
@@ -12,9 +12,59 @@ var Grid = require('mongodb').Grid;
 var Code = require('mongodb').Code;
 var BSON = require('mongodb').pure().BSON;
 var assert = require('assert');
-
 var mongoclient = new MongoClient(new Server("localhost", 27017), {native_parser: true});
 
+var getData = require('./data.js');
+
+// Used to do a fresh full import of 311 data. If a collection exists the collection is removed.
+// Scope is 2 years ago
+// TODO: Make year changeable
+function create311(data){
+  console.log("Creating new 311 data.");
+
+  //Get date of 2 years ago
+  var now = new Date();
+  now.setFullYear(now.getFullYear() - 2);
+  var start_date = now.toJSON();
+  var url = "https://secure.toronto.ca/webwizard/ws/requests.json?start_date=" 
+  + start_date+ "&jurisdiction_id=toronto.ca"; 
+
+  mongoclient.open(function(err, mongoclient) {
+
+    // Open db instance
+    var db = mongoclient.db("toronto_311_calls");
+
+    // Remove existing 311 calls. The createCollection here is
+    // just a trick to see if calls already exist. We will
+    // indiscriminately create calls after this.
+
+    //TODO: fix this part. 
+    db.createCollection('calls', {strict:true}, function(err, collection) {
+
+      if(err){
+        console.log("311 calls collection exists. Removing.")
+        db.collection('calls', function(err, collection){
+          collection.remove({}, function(err, removed){
+            assert.equal(null, err);
+            console.log("311 calls collection removed.")
+          });
+        });
+      }
+    });
+
+    db.createCollection('calls', function(err, collection) {
+      getData(url, function(data){
+        collection.insert(data, function(err, result) {
+          assert.equal(null, err);
+          console.log("Updated toronto_311_calls/call");
+        });
+      });
+    });
+  })
+}
+
+create311(null);
+/*
 mongoclient.open(function(err, mongoclient) {
 
     // Get the first db and do an update document on it
@@ -29,10 +79,12 @@ mongoclient.open(function(err, mongoclient) {
       db2.collection('mongoclient_test').update({a:1}, {b:1}, {upsert:true}, function(err, result) {
         assert.equal(null, err);
         assert.equal(1, result);
-        console.log("Updated mongoclient_test2");
+        console.log("Updated mongoclient_test2");ubun
 
         // Close the connection
         mongoclient.close();
+      });
     });
-  });
-})
+  })
+
+*/
